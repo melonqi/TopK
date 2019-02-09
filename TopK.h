23ee6cc8a150fed5e4953d1e5f7a5deb1dbae18a
melonqi <melonqi@tencent.com>
2019-02-09 14:01:08 +0800

    Update: update comments

--- TopK.h
+++ TopK.h
@@ -7,12 +7,13 @@
 
 #include <vector>
 
-template<typename T>
+template <typename T>
 
-class TopK {
-public:
-
-	TopK(int k, bool (*compare_func)(const T &t1, const T &t2)) {
+class TopK
+{
+  public:
+	TopK(int k, bool (*compare_func)(const T &t1, const T &t2))
+	{
 		k_ = k;
 		compare_ = compare_func;
 		array_.reserve(k_);
@@ -22,28 +23,33 @@ public:
 
 	void heapify(int array_size);
 
-	std::vector<T> get() {
+	std::vector<T> get()
+	{
 		return array_;
 	}
 
 	void build_heap(int array_size, int root);
 
-private:
+  private:
 	int k_;
 	std::vector<T> array_;
 	bool (*compare_)(const T &t1, const T &t2);
 };
 
-template<typename T>
-void TopK<T>::heapify(int array_size) {
-	for (int i = (array_size - 1) / 2; i >= 0; i--) {
+template <typename T>
+void TopK<T>::heapify(int array_size)
+{
+	for (int i = (array_size - 1) / 2; i >= 0; i--)
+	{
 		build_heap(array_size, i);
 	}
 }
 
-template<typename T>
-bool TopK<T>::push(const T &value) {
-	if (array_.size() < k_) {
+template <typename T>
+bool TopK<T>::push(const T &value)
+{
+	if (array_.size() < k_)
+	{
 		//array has less than k_ elements
 		array_.push_back(value);
 		heapify(array_.size());
@@ -54,9 +60,10 @@ bool TopK<T>::push(const T &value) {
 	//user define compare func
 	if (compare_ == NULL)
 		return false;
-	//if heap is min-heap, compare is greater func
-	//if heap is max-heap, compare is less func
-	if (compare_(array_[0], value)) {
+	//if heap is min-heap, compare is less func, that is array[0] < value
+	//if heap is max-heap, compare is greater func, that is array[0] > value
+	if (compare_(array_[0], value))
+	{
 		//the value need to insert heap
 		array_[0] = value;
 		heapify(k_);
@@ -64,20 +71,24 @@ bool TopK<T>::push(const T &value) {
 	return true;
 }
 
-template<typename T>
-void TopK<T>::build_heap(int array_size, int root) {
+template <typename T>
+void TopK<T>::build_heap(int array_size, int root)
+{
 	int left = 2 * root + 1;
 	int right = 2 * root + 2;
-	int largest = root;
-	if (left < array_size && compare_(array_[left], array_[largest])) {
-		largest = left;
+	int target = root;
+	if (left < array_size && compare_(array_[left], array_[target]))
+	{
+		target = left;
 	}
-	if (right < array_size && compare_(array_[right], array_[largest])) {
-		largest = right;
+	if (right < array_size && compare_(array_[right], array_[target]))
+	{
+		target = right;
 	}
-	if (largest != root) {
-		std::swap(array_[largest], array_[root]);
-		build_heap(array_size, largest);
+	if (target != root)
+	{
+		std::swap(array_[target], array_[root]);
+		build_heap(array_size, target);
 	}
 }
 
